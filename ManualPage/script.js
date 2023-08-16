849a28fc0427a6d43a012dc757aa551494583d80
Dimitris Dafnis <68849116+jim-daf@users.noreply.github.com>
2023-08-16 13:27:09 +0300

    Add files via upload

--- ManualPage/script.js
+++ ManualPage/script.js
@@ -14,3 +14,24 @@ window.addEventListener('scroll', () => {
 scrollToTopButton.addEventListener('click', () => {
   window.scrollTo({ top: 0, behavior: 'smooth' });
 });
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("downArrow").addEventListener("click", function() {
+        var calcCategories = document.getElementById("calcCategories");
+        calcCategories.style.maxHeight = calcCategories.scrollHeight + "px";
+        calcCategories.style.visibility = "visible";
+        document.getElementById("downArrow").style.display = "none";
+        document.getElementById("upArrow").style.display="block"
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("upArrow").addEventListener("click", function() {
+        var calcCategories = document.getElementById("calcCategories");
+        calcCategories.style.maxHeight = 0;
+        calcCategories.style.visibility = "hidden";
+        document.getElementById("downArrow").style.display= "block";
+        document.getElementById("upArrow").style.display="none"
+    });
+});
