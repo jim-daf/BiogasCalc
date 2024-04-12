082205c43cec9ec15fd5877b808dc48685efcd71
Dimitris Dafnis <68849116+jim-daf@users.noreply.github.com>
2024-04-12 12:11:40 +0300

    Add files via upload

--- HomePage/Functionality/script.js
+++ HomePage/Functionality/script.js
@@ -178,7 +178,7 @@ function clickEventHandler(currentTab){
                     if(document.getElementById('metapoihsh').classList.contains('changeColor') && document.getElementById('ektr_businesses')){
                         //document.getElementById('calc').removeChild(document.getElementById('numOfAnimals'))
                         document.getElementById('calc').style.position = "relative"
-                        document.getElementById('calc').style.bottom = "15px"
+                        document.getElementById('calc').style.bottom = "5px"
                         document.getElementById("ektr_businesses").remove()
                         if(document.getElementById("meta_businesses")==null){
                             document.getElementById('fieldset').insertBefore(metaB_clone,document.getElementsByClassName("tab")[1])
@@ -196,7 +196,7 @@ function clickEventHandler(currentTab){
                         */
                         //document.getElementById('calc').insertBefore(numAnimals_clone,document.getElementById('calc').childNodes[0])
                         document.getElementById('calc').style.position = "relative"
-                        document.getElementById('calc').style.bottom = "0px"
+                        document.getElementById('calc').style.bottom = "5px"
                         document.getElementById('meta_businesses').remove()
                         if(document.getElementById("ektr_businesses")==null){
                             
